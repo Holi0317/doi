5fc53a13d8fbbd8d9498ba7d20d653e469636951
Hollis Wu <whyhollis@gmail.com>
2025-10-09 09:49:41 -0400

    Add cancel signal to ky

--- src/composable/http.ts
+++ src/composable/http.ts
@@ -5,6 +5,8 @@ export function useKy(c: Context<Env>) {
   const version = c.env.CF_VERSION_METADATA;
 
   return ky.create({
+    // Cancel request when client disconnects
+    signal: c.req.raw.signal,
     headers: {
       "user-agent": `doi-app/${version.id || "unknown"}/${version.tag || "unknown"}`,
     },
