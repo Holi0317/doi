8ac7cab15463cf58fc00688bf252e3cbaf9210ad
Hollis Wu <whyhollis@gmail.com>
2025-10-09 16:01:34 -0400

    Fix ky abort signal merging

--- src/composable/http.ts
+++ src/composable/http.ts
@@ -5,10 +5,24 @@ export function useKy(c: Context<Env>) {
   const version = c.env.CF_VERSION_METADATA;
 
   return ky.create({
-    // Cancel request when client disconnects
-    signal: c.req.raw.signal,
     headers: {
       "user-agent": `doi-app/${version.id || "unknown"}/${version.tag || "unknown"}`,
+    },
+    hooks: {
+      beforeRequest: [
+        (request) => {
+          return new Request(request, {
+            // Cancel request when client disconnects
+            // ky's merge logic is broken if we specify the signal in `.create` parameter
+            // Plus this hook allows us to race/combine signals if caller also specifies one
+            signal: AbortSignal.any(
+              [request.signal, c.req.raw.signal].filter(
+                (signal) => signal !== null,
+              ),
+            ),
+          });
+        },
+      ],
     },
   });
 }
