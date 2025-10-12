705bc18dc0a3e57348c7b6dd5a3a840977b193df
Hollis Wu <whyhollis@gmail.com>
2025-10-12 13:00:05 -0400

    Rework retry strategy for providers

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:logging/logging.dart';
 import 'package:mobile/app_router.dart';
 import 'package:mobile/components/sync_worker.dart';
 import 'package:mobile/providers/logger_observer.dart';
+import 'package:mobile/repositories/retry.dart';
 
 void main() {
   Logger.root.onRecord.listen((record) {
@@ -13,25 +14,23 @@ void main() {
   });
 
   runApp(
-    const ProviderScope(
-      observers: [LoggerObserver()],
-      child: SyncWorkerWidget(child: MyApp()),
+    ProviderScope(
+      retry: retryStrategy,
+      observers: [const LoggerObserver()],
+      child: SyncWorkerWidget(
+        child: Consumer(
+          builder: (context, ref, child) {
+            return MaterialApp.router(
+              routerConfig: router,
+              // FIXME: Replace with a proper title
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+            );
+          },
+        ),
+      ),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-    );
-  }
 }
---
+++ mobile/lib/repositories/retry.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'api.dart';
+
+const _noRetryStatus = {
+  // Transient errors
+  400, 408, 500, 502, 503, 504,
+
+  // Throttling errors
+  /* 400, */ 403, 429, /* 502, 503, */ 509,
+};
+
+/// Retry strategy for riverpod provider.
+///
+/// Mostly based on AWS SDK's default retry strategy:
+/// See https://docs.aws.amazon.com/sdkref/latest/guide/feature-retry-behavior.html
+Duration? retryStrategy(int retryCount, Object error) {
+  if (error is RequestException && _noRetryStatus.contains(error.statusCode)) {
+    return null;
+  }
+
+  return ProviderContainer.defaultRetry(
+    retryCount,
+    error,
+    maxRetries: 3,
+    minDelay: const Duration(milliseconds: 100),
+    maxDelay: const Duration(seconds: 20),
+  );
+}
