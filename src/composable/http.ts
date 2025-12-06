9bbed4bda603cd117f09ad0f016928f0d2aca5b8
Hollis Wu <whyhollis@gmail.com>
2025-12-05 20:30:26 -0500

    Simplify signal handling in ky

--- src/composable/http.ts
+++ src/composable/http.ts
@@ -6,22 +6,7 @@ import ky from "ky";
  */
 export function useKy(c: Context<Env>) {
   return useBasicKy(c.env).extend({
-    hooks: {
-      beforeRequest: [
-        (request) => {
-          return new Request(request, {
-            // Cancel request when client disconnects
-            // ky's merge logic is broken if we specify the signal in `.create` parameter
-            // Plus this hook allows us to race/combine signals if caller also specifies one
-            signal: AbortSignal.any(
-              [request.signal, c.req.raw.signal].filter(
-                (signal) => signal !== null,
-              ),
-            ),
-          });
-        },
-      ],
-    },
+    signal: c.req.raw.signal,
   });
 }
 
