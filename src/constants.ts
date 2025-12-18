f802c586297456776cd672173978592d5151ac20
Hollis Wu <whyhollis@gmail.com>
2025-12-18 12:26:55 -0500

    Add request size limit
    Prepare for #14 import action

--- src/constants.ts
+++ src/constants.ts
@@ -27,3 +27,19 @@ export const MAX_EDIT_OPS = 30;
  * Most likely to pass this number into p-limit.
  */
 export const REQUEST_CONCURRENCY = 6;
+
+/**
+ * Maximum body size for incoming request. Unit is in bytes.
+ *
+ * Worker limits requests to maximum 100MB size for free plan.
+ * See https://developers.cloudflare.com/workers/platform/limits/#request-limits
+ *
+ * Most endpoints won't hit this limit (or rather, would ran out of CPU time before hitting this limit).
+ * Main concern is for import endpoints where we write the request body into KV storage.
+ *
+ * Size limit for KV is 25MB.
+ * See https://developers.cloudflare.com/kv/platform/limits/
+ *
+ * Setting this to 20MB which should be enough for most imports.
+ */
+export const MAX_BODY_SIZE = 20 * 1024 * 1024;
