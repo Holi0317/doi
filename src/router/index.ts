500bdf6c00c4f5a99dcbe4b9c496024003688e39
Hollis Wu <Holi0317@users.noreply.github.com>
2025-12-18 12:35:01 -0500

    Add request size limit (#53)
    Prepare for #14 import action

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { bodyLimit } from "hono/body-limit";
 import { clientInject } from "../middleware/client";
 import { renderer } from "../middleware/renderer";
 import { blockRequestLoop } from "../middleware/loop";
@@ -7,11 +8,13 @@ import authRouter from "./auth";
 import basicRouter from "./basic";
 import landingRouter from "./landing";
 import adminRouter from "./admin";
+import { MAX_BODY_SIZE } from "../constants";
 
 const app = new Hono<Env>({ strict: false })
   .use(clientInject(apiRouter))
   .use(renderer())
   .use(blockRequestLoop())
+  .use(bodyLimit({ maxSize: MAX_BODY_SIZE }))
   .route("/", landingRouter)
   .route("/auth", authRouter)
   .route("/api", apiRouter)
